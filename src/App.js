88e2ff2469751f983ab6873b9c5268a3eaffb67a
Edgar Martinez Arevalo <tutocpp@gmail.com>
2020-10-24 15:00:21 -0500

    update our manue item to navigate with router  HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import HomePage from "./pages/homepage/homepage.component";
+import { Route, Switch } from "react-router-dom";
+
 import "./App.css";
+
+const HatsPage = () => <div>HATS PAGE</div>;
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="shop/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
